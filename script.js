548777fb41459933f65a485e3d37052aa121644b
cwashington449 <67284647+cwashington449@users.noreply.github.com>
2024-09-01 17:16:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sun = document.querySelector('.sun');
+const sunGlow = document.querySelector('.sun-glow');
 const moon = document.querySelector('.moon');
 const sky = document.querySelector('.sky');
 
@@ -26,10 +27,18 @@ moon.addEventListener('click', () => {
 
 function animateSunToNight() {
   sun.style.transform = 'translate(500px, -200px)';
+  sun.style.backgroundColor = '#f0f0f0';
+  sunGlow.style.backgroundColor = 'rgba(255, 255, 0, 0)';
+  moon.style.display = 'block';
+  moon.style.transform = 'translate(0, 100px)';
   sky.style.backgroundColor = '#111';
 }
 
 function animateSunToDay() {
   sun.style.transform = 'translate(0, 0)';
+  sun.style.backgroundColor = 'yellow';
+  sunGlow.style.backgroundColor = 'rgba(255, 255, 0, 0.8)';
+  moon.style.display = 'none';
+  moon.style.transform = 'translate(0, -200px)';
   sky.style.backgroundColor = '#f0f0f0';
 }

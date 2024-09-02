559092c94b6a0f275e26f38a60b769680780774e
cwashington449 <67284647+cwashington449@users.noreply.github.com>
2024-09-02 09:46:41 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const themeToggle = document.getElementById('theme-toggle-check');
-
-  themeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
-  });
-});
